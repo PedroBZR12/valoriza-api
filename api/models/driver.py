05e170a4f08ed1b5de1c21d2eb8122d5b8240fc8
Amora <73441554+amoramm@users.noreply.github.com>
2026-08-31 10:13:55 -0300

    Delete api/models/driver.py

--- api/models/driver.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-class Deliverydriver(models.Model):
-  id=models.AutoField(primary_key=True)
-  driver_name=models.CharField(maxlenght=255)
-  cpf=models.CharField(maxlenght=11, unique=True)
