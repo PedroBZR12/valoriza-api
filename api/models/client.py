be831124170a18a4428249418e335d96db8cc0d7
Amora <73441554+amoramm@users.noreply.github.com>
2026-08-31 09:18:08 -0300

    Create Client model in client.py
    Add Client model with fields for client name and CPF.

--- api/models/client.py
+++ api/models/client.py
@@ -1,1 +1,5 @@
-
+from django.db import models
+class Client(models.Model):
+  id=models.AutoField(primary_key=True)
+  client_name=models.CharField(maxlenght=255)
+  cpf=models.CharField(maxlenght=11, unique=True)
