71c874ed810f23e836537d782081a84da8ee3d58
Amora <73441554+amoramm@users.noreply.github.com>
2026-08-31 10:13:46 -0300

    Delete api/models/company.py

--- api/models/company.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-class Company(models.Model):
-  id=models.AutoField(primary_key=True)
-  company_name=models.CharField(maxlenght=255)
-  cnpj=models.CharField(maxlenght=18, unique=True)
-  
