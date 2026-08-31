718a674a35ade6dcfaefbbad9935d6ea9feb8d80
Amora <73441554+amoramm@users.noreply.github.com>
2026-08-31 09:17:22 -0300

    Rename 'name' to 'company_name' in Company model

--- api/models/company.py
+++ api/models/company.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Company(models.Model):
   id=models.AutoField(primary_key=True)
-  name=models.CharField(maxlenght=255)
+  company_name=models.CharField(maxlenght=255)
   cnpj=models.CharField(maxlenght=18, unique=True)
   
