c809c45573da23b27c38bebb46dc970130374f97
Amora <73441554+amoramm@users.noreply.github.com>
2026-08-31 18:58:03 -0300

    Add Client model with fields for client data

---
+++ api/models/client.py
@@ -0,0 +1,26 @@
+#Importando a biblioteca de banco de dados django
+from django.db import models
+
+#Parâmetros de Padronização:
+#Campos de Nome e de Email aceitam 127 caracteres
+#Campos de CPF aceitam 11 caracteres
+#Campos de CNPJ aceitam 18 caracteres
+#Campos de CNH aceitam 9 caracteres
+#Campos de Endereço e de Hash de senha aceitam 255 caracteres
+
+#O método Meta vai definir estritamente o nome da tabela no django
+#e o método __str__(self) (nome horroroso, jesus) vai devolver a string 
+#nome ao invés do objeto na hora de chamar no debug.
+
+#Tabela "Cliente"
+class Client(models.Model):
+  clientName=models.CharField(max_length=127)
+  clientAdress=models.CharField(max_length=255)
+  clientCPF=models.CharField(max_length=11, unique=TRUE)
+  clientEmail=models.CharField(max_length=127, unique=TRUE)
+  clientPasswordHash=models.CharField(max_length=255)
+  clientAccountCreatedAt=models.DatetimeField(auto_now_add=True)
+  class Meta:
+    db_table="client"
+  def __str__(self):
+    return self.clientName
