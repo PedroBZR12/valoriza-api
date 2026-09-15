cf61ddcc35e968f516053129ceaf5bdffbc9eeca
PedroBZR <bzrpedro@gmail.com>
2026-09-15 15:06:41 -0300

    creating device serializer

---
+++ api/serializers/device_serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from api.models import Device
+
+class DeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at']
+
+    def validate_offered_value(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("O valor ofertado deve ser maior que zero.")
+        return value
