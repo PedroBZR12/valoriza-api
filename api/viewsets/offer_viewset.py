9fd18b2c7e273bb4ad2dd5eb2353e30a554d572b
Amora <73441554+amoramm@users.noreply.github.com>
2026-09-16 17:53:44 -0300

    Add OfferViewSet for managing offers

--- api/viewsets/offer_viewset.py
+++ api/viewsets/offer_viewset.py
@@ -1,1 +1,8 @@
+from rest_framework import viewsets
+from api.models import Offer
+from api.serializers.offer_serializer import OfferSerializer
 
+
+class OfferViewSet(viewsets.ModelViewSet):
+    queryset = Offer.objects.all().order_by('-created_at')
+    serializer_class = OfferSerializer
