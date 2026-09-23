bc716f2b83f912fb81f469c1c9ec9519f11ce15b
Amora <73441554+amoramm@users.noreply.github.com>
2026-09-23 13:08:19 -0300

    Adicionando opções estáticas de Condition e Status aos devices

--- api/models/device.py
+++ api/models/device.py
@@ -9,6 +9,18 @@ class Device(models.Model):
     offered_value = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    #Condição do aparelho
+    class Condition(models.TextChoices):
+        NEW="Novo"
+        USED="Usado"
+
+    #Status da oferta do dispositivo
+    class Status(models.TextChoices):
+        IN_ANALISYS="Em análise"
+        OFFERED="Ofertado"
+        ACCEPTED="Aceito"
+        REFUSED="Recusado"
+
     class Meta:
         db_table = "device"
 
